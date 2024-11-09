e2d98ae439800cbc0c410dbeb63e331cca3679e1
Abhisek Niraula <080bei004@ioepc.edu.np>
2024-11-09 20:25:33 +0545

    Final Commit

---
+++ src/app/faqs/faq_list.tsx
@@ -0,0 +1,55 @@
+import { IFAQ } from '@/app/faqs/faq_list_type';
+
+export const FAQList: IFAQ[] = [
+	{
+		question: 'When and where is CS50x Nepal starting?',
+		answer:
+			'CS50x Nepal is starting this soon in 2024, at Library building in IOE Purwanchal Campus.',
+	},
+	{
+		question: 'How many students can join CS50x Nepal?',
+		answer:
+			'Initially, we are planning to enroll 30 students. However, the student count can change based on interest.',
+	},
+	{
+		question: 'Is CS50x for beginners, or do I need prior coding experience?',
+		answer:
+			'CS50x is perfect for beginners! No prior coding experience is needed.',
+	},
+	{
+		question: 'How do I enroll in CS50x Nepal?',
+		answer:
+			'You can enroll in CS50x online for free. However, for CS50x Nepal, you need to apply to get into the event.',
+	},
+	{
+		question: 'What programming languages are taught in CS50x Nepal?',
+		answer:
+			'CS50x Nepal follows the same course structure as of CS50x from 2022/23.',
+	},
+	{
+		question: 'How much time should I dedicate to CS50x Nepal each week?',
+		answer:
+			'We recommend about 12–18 hours per week, but you can adjust your pace to fit your schedule.',
+	},
+	{
+		question: 'Are there assignments and exams in CS50x Nepal?',
+		answer:
+			"Yes, you'll have problem sets and quizzes to test your knowledge. They're a great way to learn!",
+	},
+	{
+		question:
+			'Can I get a certificate from Harvard for completing CS50x Nepal?',
+		answer:
+			'You will only get the free certificate offered by CS50 on completion of the course.',
+	},
+	{
+		question: 'What kind of computer or software do I need for CS50x?',
+		answer:
+			'You would need a decent laptop. In case of software, you can explore the online setup or some offline Integrated Development Environment (IDE).',
+	},
+	{
+		question: 'Can I take CS50x Nepal at my own pace?',
+		answer:
+			'Absolutely! But not keeping up to our scheduled timeline might let you not graduate the course together with others.',
+	},
+];
